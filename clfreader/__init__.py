3eb1f6cf7167247642d0a99e4e2b403874bc2d3e
Anonymous <>
2024-02-07 22:35:51 +1100

    Added log line parser

--- clfreader/__init__.py
+++ clfreader/__init__.py
@@ -4,9 +4,28 @@ A class that reads a text file in Common Log Format and converts to different fo
 
 class CLFReader:
 
+    @staticmethod
+    def parse_log(log):
+        """
+        receives a single line of CLF file and returns tuple of items
+        """
+        item1, residual = log.split(" ", 1)
+        item2, residual = residual.split(" ", 1)
+        item3, residual = residual.split(" ", 1)
+        item4, residual = residual.split("] ", 1)
+        item4 = item4.strip('[')
+        item5, residual = residual.split('\" ', 1)
+        item6, residual = residual.split(" ", 1)
+        item7, residual = residual.split(" ", 1)
+        item8, residual = residual.split(' \"', 1)
+        item8 = item8.strip('\"')
+        item9 = residual[:-1]
+        return item1, item2, item3, item4, item5, item6, item7, item8, item9
+
     def __init__(self, filename):
         with open(filename) as f:
-            self.logs = f.read()
+            logs = f.read().splitlines()
+            self.logs = [self.parse_log(log) for log in logs]
 
     def to_dataframe(self):
         pass
