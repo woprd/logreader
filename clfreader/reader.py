4ec80b8ecf08a7d5c5fc8f35d819cefd89429368
woprd <91825084+woprd@users.noreply.github.com>
2024-02-07 20:05:05 +1100

    Moved class definition to init.py

--- clfreader/reader.py
+++ clfreader/reader.py
@@ -1,17 +1,2 @@
-"""
-A class that reads a text file in Common Log Format and converts to different formats
-"""
 
-class CLFReader:
 
-    def __init__(self, filename):
-        with open(filename) as f:
-            self.logs = f.read()
-
-    def to_dataframe(self):
-        pass
-
-    def to_json(self):
-        pass 
-
-        
